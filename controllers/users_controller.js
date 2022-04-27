94e843489b03f7f7b6b61e71841a9f703c7c75b1
SKim0523 <emailsarahkim@gmail.com>
2022-04-27 15:58:52 -0400

    updated the registration.ejs form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,27 +5,67 @@ const db = require('../models')
 
 // Routes: http://localhost:3000/users
 
-// Displays form for registrating a new user
+// Displays form for registrating a "new" user
 
-router.get('/registraion', (req, res) => {
-    res.render('registration.ejs')
+router.get('/registration', (req, res) => {
+    res.render('registration.ejs');
 })
 
-// Create route for user
+//"show" route
 
-router.post('/', async (req, res, next) => {
-
+router.get('/:id', async (req, res, next) => {
     try {
-        // console.log(`The req.body is ${req.body}`)
-        const createdUser = await db.User.create(req.body);
-        console.log(`The created product is ${createdUser}`)
-        res.redirect('/profile');
+        const foundUser = await db.User.findById(req.params.id)
+        const allPost = await db.Post.find({user: req.params.id})
+        const context = { 
+            oneUser: foundUser,
+            posts: allPosts,
+        }
+        return res.render('profile.ejs', context)
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
     }
 })
+
+// router.get('/:id/edit', async (req,res, next)=>{
+//     try {
+//         const updatedUser = await db.User.findById(req.params.id);
+//         const context = {
+//             user: updatedUser
+//         }
+//         return res.render('editprof.ejs', context)
+//     } catch (error) {
+//         console.log(error);
+//         req.error = error;
+//         return next();
+//     }
+// })
+
+// Create route for user
+
+router.post('/registration', async (req, res, next) => {
+    try {
+        const temp = {
+            name: req.body.name,
+            profile_image: req.body.profile_image,
+            username: req.body.username,
+            pronouns: " ",
+            website: " ",
+            email: req.body.email,
+            bio: " "
+        };
+        // console.log(`The req.body is ${req.body}`)
+        const createdUser = await db.User.create(temp);
+        // console.log(`The created product is ${createdUser}`)
+        res.redirect('/profile');
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
 
 
 //I need to get access to the profile page
