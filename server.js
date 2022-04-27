29b3cf126405a4dfbdd3e56afefb9e05d069cfad
SKim0523 <emailsarahkim@gmail.com>
2022-04-26 22:44:34 -0400

    create registration.ejs, tour.ejs; add boilerplate and get/post route in users_controller.js

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const controllers = require('./controllers');
+const methodOverride = require('method-override')
+
 require('./config/db.connection')
 
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false }))
+app.use('/users', controllers.users)
+
+app.set('view engine', 'ejs')
+
+app.get('/tour', (req, res) => {
+    res.render('tour.ejs')
+})
+
+
 app.listen(PORT, () => console.log (`Listening on port: ${PORT}`));
+
+
+// | Method | Route         | Description             |
+// | ------ | ------------- | ----------------------- |
+// | GET    | /             | Read all posts          |
+// | GET    | /state/:state | Read all posts by state |
+// | POST   | /             | Create post\*           |
+// | PUT    | /id/:id       | Update entire post\*    |
+// | DELETE | /id/:id       | Delete post\*           |
+
+// Welcome Page >> /user/registration
+// Tour Page >> /tour
+// Index Page >> /user/profile
