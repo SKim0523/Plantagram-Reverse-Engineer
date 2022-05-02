1b7f5f4df70c36d75550f0e4fd587f4c17e537da
SKim0523 <emailsarahkim@gmail.com>
2022-05-02 14:19:11 -0400

    linked home icons on Post show page and Edit Profile page back to the Profile page.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router =  express.Router();
-const db = require('../models')
-
+const db = require('../models');
 
 // Routes: http://localhost:3000/users
 
@@ -9,42 +8,39 @@ const db = require('../models')
 router.get('/tour/:id', async (req, res, next) => {
     try {
         const touringUser= await db.User.findById(req.params.id);
-        // console.log(touringUser)
         const context = { 
             user: touringUser
-        }
-        res.render('tour.ejs', context)
+        };
+        res.render('tour.ejs', context);
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    }
-})
+    };
+});
 
-// (0) Directs user from Tour page to Profile Page
+// Directs user from Tour page to Profile Page
 router.get('/tour/:id', async (req, res, next) => {
     try {
         const touringUser= await db.User.findById(req.params.id);
         const context = { 
             user: touringUser
-        }
-        return res.redirect(`/users/profile/${user}`)
-        console.log(req.body)
+        };
+        return res.redirect(`/users/profile/${user}`);
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    }
-})
+    };
+});
 
-// (1) Displays form for registrating a "new" user
+// Displays form for registrating a new user
 
 router.get('/registration', (req, res) => {
     res.render('registration.ejs');
-})
+});
 
-// (2) 'Create Account' button; Creates a user with the help of (1)
-
+// Create Account button
 router.post('/registration', async (req, res, next) => {
     try {
         const temp = {
@@ -56,77 +52,57 @@ router.post('/registration', async (req, res, next) => {
             email: req.body.email,
             bio: ""
         };
-        // console.log(`The req.body is`, req.body)
         const createdUser = await db.User.create(temp);
-        // const foundUser = await db.User.findById(req.params.id)
-        // console.log(createdUser)
-        // console.log(createdUser._id.valueOf())
-        const id=createdUser._id.valueOf()
-        // console.log(`The created product is ${createdUser}`)
-        // const context = { 
-        //     oneUser: createdUser,
-        // }
-        // res.render('/profile.ejs',context);
+        const id=createdUser._id.valueOf();
         res.redirect(`/users/profile/${id}`);
-        // setTimeout(() => {res.redirect(`/users/profile/${id}`)}, 1000)
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    }
+    };
 });
 
-// (3) Displays user's profile page
-
+// Displays user's profile page
 router.get('/profile/:id', async (req, res, next) => {
     try {
-        const foundUser = await db.User.findById(req.params.id).populate('posts')
-        // const allPosts = await db.Post.find({user: req.params.id})
-        console.log(foundUser)
+        const foundUser = await db.User.findById(req.params.id).populate('posts');
         const context = { 
             oneUser: foundUser,
             posts: foundUser.posts
-        }
-        return res.render('profile.ejs', context)
-        // console.log(context)
-        // console.log(foundUser)
-        // console.log(req.params.id)
+        };
+        return res.render('profile.ejs', context);
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    }
-})
+    };
+});
 
-// (4) "Edit Profile" link; Form for editing user
+// Edit Profile link; Form for editing user
 router.get('/profile/edit/:id', async (req,res, next)=>{
     try {
         const updatedUser = await db.User.findById(req.params.id);
         const context = {
             user: updatedUser
-        }
-        return res.render('editprof.ejs', context)
+        };
+        return res.render('editprof.ejs', context);
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    }
-})
+    };
+});
 
-// (5) "Save Button" on Edit Profile Page; 
+// Save Button on Edit Profile Page; 
 router.put('/profile/edit/:id', async (req, res, next) => {
     try {
         const updatedUser = await db.User.findByIdAndUpdate(req.params.id, req.body);
-        // console.log(req.body);
-        // console.log(updatedUser);
-        return res.redirect(`/users/profile/${req.params.id}`)
-        console.log(req.body)
+        return res.redirect(`/users/profile/${req.params.id}`);
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    }
-})
-
+    };
+});
 
 module.exports = router
